ed796ec60075faa928beb619e896f07381f23946
MountainBean <samueld.work@gmail.com>
2025-02-18 12:18:51 +1300

    Some cleaning and reformatic on syntax to align with modern C++ best practises and my own preferences

--- LL/ll.cpp
+++ LL/ll.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 
-//! (Singly) Linked List Node Class.
+//! (Singly) Linked list node struct
+/*! Simple data aggregate to hold each node in a linked list
+ */
+struct Node {
 
-class Node {
-public:
+    int value {};           // an integer value
+    Node* next {nullptr};   // null pointer which will point to the next node
 
-    int value;          /*! an integer value */
-    Node* next;         /*! null pointer which will point to the next node */
-
-    //! Constructor class. Initialises the Node value to the given integer.
-    Node(int value){
-        this->value = value;
-        next = nullptr;
-    };
 };
 
 //! Simple implementation of a linked list using standard C++
@@ -20,29 +15,29 @@ public:
 class LinkedList {
 
 private:
-    Node* head {nullptr};       /*! pointer to the first Node in the Linked List. */
-    Node* tail {nullptr};       /*! pointer to the last Node in the Linked List. */
-    int length;                 /*! The length of the Linked List. DON'T FORGET TO UPDATE
-                                    THIS IN CLASS METHODS. */
+    Node* m_head {nullptr};       /*! pointer to the first Node in the Linked List. */
+    Node* m_tail {nullptr};       /*! pointer to the last Node in the Linked List. */
+    int m_length;                 /*! The length of the Linked List. DON'T FORGET TO UPDATE
+                                    THIS IN MEMBER FUNCTIONS. */
 
 public:
     //! Constructor method. Initialises the List with a Node containing the given integer.
     LinkedList(int value){
-        Node* newNode = new Node(value);
-        head = newNode;
-        tail = newNode;
-        length = 1;
+        Node* newNode = new Node {value};
+        m_head = newNode;
+        m_tail = newNode;
+        m_length = 1;
     }
 
     //! Destructor method. Iterates along the list, releasing each Node from memory.
     /*! O(n). This method iterates through each member of of the list making it O(n) 
      * where n = the number of Nodes in the list. */
     ~LinkedList(){
-        Node* temp {head};
-        while(head){
-            head = head->next;
+        Node* temp {m_head};
+        while(m_head){
+            m_head = m_head->next;
             delete temp;
-            temp = head;
+            temp = m_head;
         }
     }
 
@@ -50,7 +45,7 @@ public:
     /*! O(n). This method iterates through each member of of the list making it O(n) 
      * where n = the number of Nodes in the list. */
     void printList() {
-        Node* temp {head};
+        Node* temp {m_head};
         while(temp) {
             std::cout << temp->value << "\n";
             temp = temp->next;
@@ -58,97 +53,102 @@ public:
     }
 
     //! Returns the int value of the Node at the head of the List.
-    void getHead() {
-        std::cout << "Head: " << head->value << "\n";
+    int getHead() {
+        return m_head->value;
     }
 
     //! Returns the int value of the Node at the tail of the List.
-    void getTail() {
-        std::cout << "Tail: " << tail->value <<  "\n";
+    int getTail() {
+        return m_tail->value;
     }
 
     //! Returns the total number of Nodes in the List.
-    void getLength() {
-        std::cout << "Length: " << length << "\n";
+    int getLength() {
+        return m_length;
     }
 
     //! Adds the given value to the end of the List in a Node.
     /*! O(1). This method will use the tail pointer to alter the last Node in the
      * List. No iteration is needed so complexity is constant, regardless of the 
-     * length of the List. */
+     * length of the List.
+     */
     bool append(int value) {
-        Node* newNode = new Node(value);
-        if (length == 0){
-            head = newNode;
-            tail = newNode;
+        Node* newNode = new Node {value};
+        if (m_length == 0){
+            m_head = newNode;
+            m_tail = newNode;
         }
         else {
-            tail->next = newNode;
-            tail = newNode;
+            m_tail->next = newNode;
+            m_tail = newNode;
         }
-        length++;
+        ++m_length;
         return(true);
     }
 
     //! Deletes the last Node in the List.
     /*! O(n). This method must iterate through each member of the list to find 
      * the 2nd-to-last item. Making it O(n) where n = the number of Nodes in the 
-     * list. */
+     * list. 
+     */
     void deleteLast(){
-        if (length == 0 ) return;
-        Node* temp {head};
-        if (length == 1 ) {
-            head = nullptr;
-            tail = nullptr;
+        if (m_length == 0 ) return;
+        Node* temp {m_head};
+        if (m_length == 1 ) {
+            m_head = nullptr;
+            m_tail = nullptr;
         } else {
-            while(temp->next != tail){
+            while(temp->next != m_tail){
                 temp = temp->next;
             }
-            tail = temp;
-            temp = tail->next;
-            tail->next = nullptr;
+            m_tail = temp;
+            temp = m_tail->next;
+            m_tail->next = nullptr;
         }
         delete temp;
-        length--;
+        --m_length;
     }
 
     //! Removes the first value from the front of the List.
     /*! O(1). This method will use the head pointer to alter the first Node in the
      * List. No iteration is needed so complexity is constant, regardless of the 
-     * length of the List. */
+     * length of the List.
+     */
     void deleteFirst(){
-        if (length == 0 ) return;
-        Node* temp {head};
-        if (length == 1 ) {
-            head = nullptr;
-            tail = nullptr;
+        if (m_length == 0 ) return;
+        Node* temp {m_head};
+        if (m_length == 1 ) {
+            m_head = nullptr;
+            m_tail = nullptr;
         } else {
-            head = head->next;
+            m_head = m_head->next;
         }
         delete temp;
-        length--;
+        --m_length;
     }
 
     //! Adds the given value to the front of the List.
     /*! O(1). This method will use the head pointer to alter the first Node in the
      * List. No iteration is needed so complexity is constant, regardless of the 
-     * length of the List. */
+     * length of the List. 
+     */
     void prepend(int value){
-        Node* newNode = new Node(value);
-        newNode->next = head;
-        head = newNode;
-        if (length == 0) tail = newNode;
-        length++;
+        Node* newNode = new Node {value};
+        newNode->next = m_head;
+        m_head = newNode;
+        if (m_length == 0) m_tail = newNode;
+        ++m_length;
     }
 
     //! Returns the Node at the position given.
     /*! O(n). Iterates through the list until it reaches the index of the requested
-     * Node making it O(n) where n = index */
+     * Node making it O(n) where n = index.
+     */
     Node* get(int index){
-        if (index < 0 || index >= length) {
+        if (index < 0 || index >= m_length) {
             return nullptr;
         }
-        Node* temp {head};
+        Node* temp {m_head};
         for (int i {0}; i < index; i++){
             temp = temp->next;
         }
@@ -157,7 +157,8 @@ public:
 
     //! Change the value of the Node at the given index.
     /*! O(n). Iterates through the list until it reaches the index of the requested
-     * Node making it O(n) where n = index */
+     * Node making it O(n) where n = index.
+     */
     bool set(int index, int value){
         Node* temp = get(index);
         if (temp) {
@@ -169,64 +170,67 @@ public:
 
     //! inserts the given value in a Node before the given index in the List
     /*! O(n). Iterates through the list until it reaches the index of the requested
-     * Node making it O(n) where n = index */
+     * Node making it O(n) where n = index.
+     */
     bool insert(int index, int value){
-        if (index < 0 || index > length) return false;
+        if (index < 0 || index > m_length) return false;
         if (index == 0) {
             prepend(value);
             return true;
         }
-        if (index == length) {
+        if (index == m_length) {
             append(value);
             return true;
         }
 
-        Node* newNode = new Node(value);
+        Node* newNode = new Node {value};
         Node* temp {get(index-1)};
 
         newNode->next = temp->next;
         temp->next = newNode;
-        length++;
+        ++m_length;
         return true;
     }
 
     //! Deletes the Node at the given index.
     /*! O(n). Iterates through the list until it reaches the index of the requested
-     * Node making it O(n) where n = index */
+     * Node making it O(n) where n = index.
+     */
     void deleteNode(int index){
-        if ( index < 0 || index >= length ) return;
+        if ( index < 0 || index >= m_length ) return;
         if ( index == 0 ) deleteFirst();
-        if ( index == length-1 ) deleteLast();
+        if ( index == m_length-1 ) deleteLast();
 
         Node* prev {get(index-1)};
         Node* temp {prev->next};
 
         prev->next = temp->next;
         delete temp;
-        length--;
+        ++m_length;
     }
 
     //! flips the List around so that head is tail and tail is head.
     /*! O(n). The List must be iterated down so the complexity is O(n) where 
-     * n = length of the List.*/
+     * n = length of the List.
+     */
     void reverse() {
-        Node* temp {head};
-        Node* after {head};
+        Node* temp {m_head};
+        Node* after {m_head};
         Node* before {nullptr};
-        for (int i {0}; i < length; i++){
+        for (int i {0}; i < m_length; i++){
             after = temp->next;
             temp->next = before;
             before = temp;
             temp = after;
         }
-        tail = head;
-        head = before;
+        m_tail = m_head;
+        m_head = before;
     }
 
     //! Finds and returns the middle Node in the List.
     Node* middle() {
-        Node* tortoise {head};
-        Node* hare {head};
+        Node* tortoise {m_head};
+        Node* hare {m_head};
         while (hare && hare->next) {
             hare = hare->next->next;
             tortoise = tortoise->next;
